a74f5892cb7e5e145122081c0061b6ff16bb93d3
teng <2352124452@qq.com>
2020-11-26 15:11:32 +0800

    pojo 添加

---
+++ fresh-system-po-6655/src/main/java/com/aaa/fresh/pojo/ProcurementRequirementData.java
@@ -0,0 +1,26 @@
+package com.aaa.fresh.pojo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProcurementRequirementData {
+
+  private String id;
+  private String name;
+  private String buyer;
+  private String requirementStatus;
+  private Date createTime;
+  private Date endTime;
+  //private String approvalstatus;
+  private Integer version;
+  private Integer page; //页码
+  private Integer size; //大小
+
+
+}
